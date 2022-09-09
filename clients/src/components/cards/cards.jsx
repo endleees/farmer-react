64ffbca5481ed4f674a6fe5b58de78276a2f26ef
endleees <fess0506@gmail.com>
2022-09-10 02:39:53 +0300

    Цвета для карточек

--- clients/src/components/cards/cards.jsx
+++ clients/src/components/cards/cards.jsx
@@ -7,7 +7,11 @@ function Cards ({mokeCards}){
         let array=[];
         for(let i=0;i<mokeCards[stat].imgSrc.length;i++){
             
-            const card= <li className={`cards__item cards__item--${stat}`}><img className="cards__item-image" src={mokeCards[stat].imgSrc[i]}/><h3 className="cards__title">{mokeCards[stat].title[i]}</h3></li> ;
+            const card= <li className={`cards__item cards__item--${stat}`}><img className="cards__item-image" src={mokeCards[stat].imgSrc[i]} width={mokeCards[stat].imgWidth[i]} height={mokeCards[stat].imgHeight[i]}/>
+                <p className={`cards__title-status cards__title-status--${stat}`}>{mokeCards[stat].titleStatus}</p>
+                <h3 className="cards__title">{mokeCards[stat].title[i]}</h3>
+                <p className="cards__description">{mokeCards[stat].description[i]}</p>
+            </li> ;
             array.push(card);
         }
         return array
@@ -16,7 +20,6 @@ function Cards ({mokeCards}){
         <section className="cards">
             <TitleCards/>
             <ul className="cards__list">
-                
                     {arrayCards({mokeCards},"farmer")}
                     {arrayCards({mokeCards},"shop")}
             </ul>
